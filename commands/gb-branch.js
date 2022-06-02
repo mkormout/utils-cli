53f5cb81dc82f5f610f392c8d18ceef8ea95a4ce
kormout <milan.kormout@drmax.cz>
2022-06-02 12:19:50 +0200

    added: git branch

---
+++ commands/gb-branch.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk')
+const { options, processGitFolders } = require('../common/gb-common')
+
+module.exports = {
+  name: 'gbbr',
+  description: 'Git branch batch operation on multiple git folders.',
+  arguments: [
+    { name: '<branch-name>', description: 'name of desired git branch' },
+  ],
+  options: [
+    ...options,
+    { flags: '-D, --delete-branch', description: 'Deletes a branch.', default: false },
+  ],
+  action: (branchName, { deleteBranch, pattern, verbose }) => {
+    return processGitFolders(
+      pattern,
+      verbose,
+      async (git, folder) => {
+
+      if (deleteBranch) {
+        console.log(`Delete branch ${chalk.green(branchName)} on ${chalk.yellow(folder)}`)
+        await git.branch( ['-D', branchName])
+      } else {
+        console.log(`Create branch ${chalk.green(branchName)} on ${chalk.yellow(folder)}`)
+        await git.branch(branchName)
+      }
+    })
+  }
+}
