33c68bdf2a4054f9554c97d01d35fd0e89a43000
kormout <milan.kormout@drmax.cz>
2022-11-29 21:18:44 +0100

    fixed: git batch push set-upstream

--- commands/gb-push.js
+++ commands/gb-push.js
@@ -6,15 +6,22 @@ module.exports = {
   description: 'Git push batch operation on multiple git folders.',
   options: [
     ...options,
+    { flags: '-u, --setupstream <setupstream...>', description: 'Set upstream branch.' },
   ],
 
-  action: async ({ pattern, verbose }) => {
+  action: async ({ pattern, verbose, setupstream, remote, branch }) => {
     return processGitFolders(
       pattern,
       true,
       async (git, folder) => {
-        console.log(`Pushing ${chalk.yellow(folder)}`)
-        await git.push()
+        if (setupstream) {
+          const [remote, branch] = setupstream
+          console.log(`Pushing ${chalk.yellow(folder)} to ${chalk.blue(remote)} as ${chalk.green(branch)}`)
+          await git.push(['-u', remote, branch])
+        } else {
+          console.log(`Pushing ${chalk.yellow(folder)}`)
+          git.push()
+        }
       }
     )
   }
